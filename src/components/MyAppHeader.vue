ca07bf9937944322ead480caea9d02023f39e1bc
FilippoBonanno <fbonanno15@gmail.com>
2024-06-04 12:38:04 +0200

    header preview

--- src/components/MyAppHeader.vue
+++ src/components/MyAppHeader.vue
@@ -1,4 +1,5 @@
 <script>
+
 export default {
     name: "MyAppHeader",
     data() {
@@ -13,7 +14,159 @@ export default {
 </script>
 
 <template>
-    <h1>Questo è l'header</h1>
+    <!-- CONTAINER PRINCIPALE CON SFONDO -->
+    <div class="fluid bgHeader">
+        <!-- NAVBAR -->
+        <nav class="navbar navbar-light bg-trasparent p-5">
+            <div class="containerNav d-flex">
+                <!-- LOGO NAV -->
+                <div>
+                    <a class="navbar-brand" href="#">
+                        <img src="../assets/img/logo-img-01.png" width="25" height="25" class="logoHeader align-top">
+
+                    </a>
+                </div>
+
+                <!-- DROPDOWN LINK NAV -->
+                <div class="dropdown d-flex">
+
+
+                    <div class="text-dark">⟶</div>
+
+                    <div class="dropdown">
+                        <a class="dropdown-item pe-4 text-dark fw-bold" href="#" id="dropdownHomes"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            HOMES
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="dropdownHomes">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+
+
+                    <div class="text-dark">⟶</div>
+                    <div class="dropdown">
+                        <a class="dropdown-item pe-4 text-dark fw-bold" href="#" id="dropdownHomes"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            HOMES
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="dropdownHomes">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+
+
+                    <div class="text-dark">⟶</div>
+                    <div class="dropdown">
+                        <a class="dropdown-item pe-4 text-dark fw-bold" href="#" id="dropdownHomes"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            HOMES
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="dropdownHomes">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+
+                    <div class="text-dark">⟶</div>
+                    <div class="dropdown">
+
+                        <a class="dropdown-item pe-4 text-dark fw-bold" href="#" id="dropdownHomes"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            HOMES
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="dropdownHomes">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+
+                    <div class="text-dark">⟶</div>
+                    <div class="dropdown">
+
+                        <a class="dropdown-item pe-4 text-dark fw-bold" href="#" id="dropdownHomes"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            HOMES
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="dropdownHomes">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+
+                    <div class="text-dark arrow">⟶</div>
+                    <div class="dropdown">
+
+                        <a class="dropdown-item animated pe-4 text-dark fw-bold" href="#" id="dropdownHomes"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            HOMES
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="dropdownHomes">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+
+                    <a class="dropdown-item pe-4 text-dark fw-bold" href="#">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </a>
+
+                    <a class="dropdown-item pe-3 text-dark fw-bold" href="#">
+                        <i class="fa-solid fa-bars"></i>
+                    </a>
+
+
+
+
+                </div>
+
+
+            </div>
+        </nav>
+    </div>
+
 </template>
 
-<style scoped></style>
+<style scoped>
+.containerNav {
+    margin-left: 2rem;
+    width: 100%;
+    justify-content: space-between;
+    flex-wrap: inherit;
+    align-items: center;
+}
+
+.bgHeader {
+    height: 100vh;
+    background-image: url(../assets/img/rev-slider-main-home-img-02.png);
+}
+
+.logoHeader {
+    width: 100%;
+
+}
+
+.arrow {
+    opacity: 0;
+    transition: 800ms;
+}
+
+.animated:hover + .arrow {
+    opacity: 1;
+}
+
+</style>
